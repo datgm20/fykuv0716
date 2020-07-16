b864521b337d82765b6314a694d54f36aa24ce8d
Yu Tanaka <YRK00337@nifty.com>
2020-07-16 10:33:08 +0900

    完成

--- fykuv0716/Form1.cs
+++ fykuv0716/Form1.cs
@@ -16,5 +16,11 @@ namespace fykuv0716
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
     }
 }
